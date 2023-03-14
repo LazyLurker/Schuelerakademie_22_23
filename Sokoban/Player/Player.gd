2782676b036b8f8a807b4abed62aa18dfef0cb0a
LazyLurker <127869383+LazyLurker@users.noreply.github.com>
2023-03-14 15:41:43 +0000

    This implements the feature of a move query, which queries the next move the player makes. This results in a smoother gameplay, especially suitable for speedruns, but also for more pleasant gameplay.

--- Sokoban/Player/Player.gd
+++ Sokoban/Player/Player.gd
@@ -1,21 +1,25 @@
 extends "res://Template/Moveable.gd"
 
 var last_position = Vector2()
+var query = null
 
 func _ready():
 	last_position = position
 
-
 func _unhandled_input(event):
-	if event.is_action_pressed("Up"):
-		move(Vector2.UP)
-	elif event.is_action_pressed("Right"):
-		move(Vector2.RIGHT)
-	elif event.is_action_pressed("Down"):
-		move(Vector2.DOWN)
-	elif event.is_action_pressed("Left"):
-		move(Vector2.LEFT)
-
+	if event.is_action_pressed("Up") and query == null:
+		query = Vector2.UP
+	elif event.is_action_pressed("Right") and query == null:
+		query = Vector2.RIGHT
+	elif event.is_action_pressed("Down") and query == null:
+		query = Vector2.DOWN
+	elif event.is_action_pressed("Left") and query == null:
+		query = Vector2.LEFT
+		
+func _process(_delta):
+	if query != null:
+		move(query)
+		
 func move(direction):
 	if moving:
 		return
@@ -34,6 +38,7 @@ func move(direction):
 			0
 		)
 		$Tween.start()
+	query = null
 
 
 func iAmBlocked():
